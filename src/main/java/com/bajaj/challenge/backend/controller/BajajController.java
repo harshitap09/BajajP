07f8b1013c061b7911a46b60d5de8b91e6a3a043
harshita09 <harshitapanwar0910gmail.com>
2025-05-10 17:07:30 +0530

    Initial commit

---
+++ src/main/java/com/bajaj/challenge/backend/controller/BajajController.java
@@ -0,0 +1,36 @@
+package com.bajaj.challenge.backend.controller;
+
+import com.bajaj.challenge.backend.model.RequestData;
+import com.bajaj.challenge.backend.model.ResponseData;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class BajajController {
+
+    @PostMapping("/bfhl")
+    public ResponseData processData(@RequestBody RequestData requestData) {
+        List<String> numbers = new ArrayList<>();
+        List<String> alphabets = new ArrayList<>();
+
+        for (String item : requestData.getData()) {
+            if (item.matches("\\d+")) {
+                numbers.add(item);
+            } else {
+                alphabets.add(item);
+            }
+        }
+
+        ResponseData response = new ResponseData();
+        response.setIs_success(true);
+        response.setUser_id("your_name_ddmmyyyy");
+        response.setEmail("your@email.com");
+        response.setRoll_number("YourRoll123");
+        response.setNumbers(numbers);
+        response.setAlphabets(alphabets);
+
+        return response;
+    }
+}
---
+++ src/main/java/com/bajaj/challenge/backend/model/RequestData.java
@@ -0,0 +1,15 @@
+package com.bajaj.challenge.backend.model;
+
+import java.util.List;
+
+public class RequestData {
+    private List<String> data;
+
+    public List<String> getData() {
+        return data;
+    }
+
+    public void setData(List<String> data) {
+        this.data = data;
+    }
+}
